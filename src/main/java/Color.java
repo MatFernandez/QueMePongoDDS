c20b9331399fc2d1ec36d00d199ff16bf587a9c4
Matias Fernandez <mattifernan19@gmail.com>
2022-04-29 15:39:52 -0300

    Validaciones RGB

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,14 +1,23 @@
 
 public class Color {
 
-  Integer red;
-  Integer green;
-  Integer blue;
+  Integer rojo;
+  Integer verde;
+  Integer azul;
 
-  public Color(Integer red, Integer green, Integer blue) {
-    this.red = red;
-    this.green = green;
-    this.blue = blue;
+  public Color(Integer rojo, Integer verde, Integer azul) {
+    if(rojo > 256 || rojo < 0){
+      throw new RuntimeException("El valor del color rojo debe estar comprendido entre 0 y 256");
+    }
+    if(verde > 256 || verde < 0){
+      throw new RuntimeException("El valor del color verde debe estar comprendido entre 0 y 256");
+    }
+    if(azul > 256 || azul < 0){
+      throw new RuntimeException("El valor del color azul debe estar comprendido entre 0 y 256");
+    }
+    this.rojo = rojo;
+    this.verde = verde;
+    this.azul = azul;
   }
 
 }
